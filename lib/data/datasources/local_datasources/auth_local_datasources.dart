0e78d0385aa362cd88d205ea5677873fe6172ae8
RaffyJO <rafiteguh6@gmail.com>
2024-10-21 07:39:08 +0700

    feat: auth service

--- lib/data/datasources/local_datasources/auth_local_datasources.dart
+++ lib/data/datasources/local_datasources/auth_local_datasources.dart
@@ -1,1 +1,55 @@
-class AuthLocalDatasources {}
+import 'dart:convert';
+
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:presience_app/data/dto/requests/auth_dto.dart';
+import 'package:presience_app/data/dto/responses/auth_response.dart';
+import 'package:presience_app/domain/entities/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthLocalDataSource {
+  Future<void> saveAuthData(AuthResponse authResponse) async {
+    final Map<String, dynamic> decodedToken =
+        JwtDecoder.decode(authResponse.token!);
+    final user = User(
+      nim: decodedToken['data']['nim'],
+      name: decodedToken['data']['name'],
+      birthDate: decodedToken['data']['birth_date'],
+      gender: decodedToken['data']['gender'],
+      avatar: decodedToken['data']['avatar'],
+      major: decodedToken['data']['major'],
+      semester: decodedToken['data']['semester'],
+      isVerified: decodedToken['data']['verified'],
+    );
+
+    final authData = AuthDto(
+      token: authResponse.token,
+      user: user,
+    );
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_data', jsonEncode(authData.toJson()));
+  }
+
+  Future<void> removeAuthData() async {
+    // Remove auth data from local storage
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('auth_data');
+  }
+
+  Future<AuthDto?> getAuthData() async {
+    // Get auth data from local storage
+    final prefs = await SharedPreferences.getInstance();
+    final authData = prefs.getString('auth_data');
+    if (authData != null) {
+      return AuthDto.fromJson(jsonDecode(authData));
+    } else {
+      return null;
+    }
+  }
+
+  Future<bool> isUserLoggedIn() async {
+    // Check if user is logged in
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.containsKey('auth_data');
+  }
+}
